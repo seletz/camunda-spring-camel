41b33829cca990779662390bab82b8a4cbc8105f
Stefan Eletzhofer <se@nexiles.de>
2018-05-29 18:23:48 +0200

    Initial commit

---
+++ src/main/java/com/nexiles/camundacamel/CamundaCamelApplication.java
@@ -0,0 +1,12 @@
+package com.nexiles.camundacamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaCamelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaCamelApplication.class, args);
+    }
+}
